52464cc20814e9c33d92b251c9d7872c64b2bf71
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2022-10-26 01:52:58 +0600

    Created Sidebar for Courses Outlet.

---
+++ src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Sidebar = ({ courses }) => {
+	return (
+		<div className="ml-6 lg:sticky lg:top-[165px]">
+			<h1 className="text-center text-4xl font-medium text-blue-400 mb-6">
+				<Link to="/courses">All Courses</Link>
+			</h1>
+			<div className="ml-8">
+				{courses.map((course) => (
+					<li
+						key={course.id}
+						className="list-decimal mb-3 text-lg font-medium hover:text-blue-600"
+					>
+						<Link to={`course/${course.id}`}>{course.name}</Link>
+					</li>
+				))}
+			</div>
+		</div>
+	);
+};
+
+export default Sidebar;
