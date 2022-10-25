45c8c41ec87e1de0328bb2e11a4ad3147816b539
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2022-10-25 12:58:56 +0600

    Added Navbar Component to the Main Layout.

--- src/Layouts/Main/Main.js
+++ src/Layouts/Main/Main.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from '../../Components/Navbar/Navbar';
 
 const Main = () => {
 	return (
 		<div>
-			<h1>This is Main Layout.</h1>
+			<Navbar />
+			<Outlet />
 		</div>
 	);
 };
