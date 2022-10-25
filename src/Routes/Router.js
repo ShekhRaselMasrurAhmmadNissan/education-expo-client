f659f2732e0c91c8787baef7f5a5a413e7a47fdb
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2022-10-25 22:56:22 +0600

    Added Home route to the Router.

--- src/Routes/Router.js
+++ src/Routes/Router.js
@@ -1,9 +1,18 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Main from '../Layouts/Main/Main';
 import ErrorPage from '../Pages/ErrorPage/ErrorPage';
+import Home from '../Pages/Home/Home';
 
 const router = createBrowserRouter([
-	{ path: '/', element: <Main />, errorElement: <ErrorPage /> },
+	{
+		path: '/',
+		element: <Main />,
+		errorElement: <ErrorPage />,
+		children: [
+			{ index: true, element: <Home /> },
+			{ path: 'home', element: <Home /> },
+		],
+	},
 ]);
 
 export default router;
