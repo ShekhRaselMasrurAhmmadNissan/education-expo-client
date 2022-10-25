28e2ace2976744e2debe3c25854681640b752bfb
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2022-10-25 12:03:40 +0600

    Created and implemented Router.

---
+++ src/Routes/Router.js
@@ -0,0 +1,6 @@
+import { createBrowserRouter } from 'react-router-dom';
+import Main from '../Layouts/Main/Main';
+
+const router = createBrowserRouter([{ path: '/', element: <Main /> }]);
+
+export default router;
