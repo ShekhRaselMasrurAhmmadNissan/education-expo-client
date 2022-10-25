e71d1d4a260e9b5c9baf7379d50d1a84797e73bf
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2022-10-26 01:52:29 +0600

    Created Courses Outlet.

---
+++ src/Layouts/Courses/Courses.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Outlet, useLoaderData } from 'react-router-dom';
+import Sidebar from '../../Components/Sidebar/Sidebar';
+
+const Courses = () => {
+	const courses = useLoaderData();
+	console.log(courses);
+	return (
+		<div className="grid grid-cols-7 mt-8">
+			<div className="col-span-2">
+				<Sidebar courses={courses} />
+			</div>
+			<div className="col-span-5">
+				<Outlet />
+			</div>
+		</div>
+	);
+};
+
+export default Courses;
